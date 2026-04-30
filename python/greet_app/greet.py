b1e19077f8c322594796ab2d8186696cb5203fd3
Alex Jayachandran <alexjayachandran1@gmail.com>
2026-04-30 19:13:47 +0530

    added flask

--- python/greet_app/greet.py
+++ python/greet_app/greet.py
@@ -1,6 +1,19 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
 def greet(name):
     return f"Hello, {name}!"
 
+@app.route("/")
+def home():
+    return "Greet API is running 🚀"
+
+@app.route("/greet", methods=["GET"])
+def greet_api():
+    name = request.args.get("name", "Guest")
+    message = greet(name)
+    return jsonify({"message": message})
+
 if __name__ == "__main__":
-    user = input("Enter your name: ")
-    print(greet(user))
+    app.run(debug=True)
